c7ca3b6f305723b4a781ac227b853d0cffc8f1de
wete <williamtan1232@gmail.com>
2023-10-14 17:22:16 +0700

    deploy

--- bun.co/src/pages/home/HomePage.jsx
+++ bun.co/src/pages/home/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import FirstSection from './partialsHome/FirstSection'
-import SecondSection from './partialsHome/secondsection'
+import SecondSection from './partialsHome/SecondSection'
 import ThirdSection from './partialsHome/ThirdSection'
 import FourthSection from './partialsHome/FourthSection'
 import LimitedOffer from './partialsHome/LimitedOffer'
